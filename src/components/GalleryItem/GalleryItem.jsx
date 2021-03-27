28e49c69bccbb37c1925994436eff459425f10d9
Anthony Moe <anthony.moe7@gmail.com>
2021-03-27 14:42:44 -0500

    photo and description toggle work styling needs work, like button needs a put route

--- src/components/GalleryItem/GalleryItem.jsx
+++ src/components/GalleryItem/GalleryItem.jsx
@@ -1,11 +1,26 @@
+import { useState } from 'react';
+
 function GalleryItem (props) {
+    const[showPhoto, setShowPhoto] = useState( false );
+
+    let displayPhoto = () => {
+        let content = <img className="photo" onClick = {toggleContent} src={props.photo.path } />
+        if( showPhoto ) {
+            content = <div className="description" onClick = {toggleContent}><p>{props.photo.description}</p></div> 
+        }
+        return content;
+    }
+
+    let toggleContent = () => {
+        setShowPhoto( !showPhoto );
+    }
+
     return(
         <>
             <div className="content">
-                <img className="photo" src={props.photo.path} />
-                <p>{props.photo.description}</p>
+                {displayPhoto()}
                 <button id={props.photo.id}>Love IT!</button>
-                <p>Likes:<span></span></p>
+                <p>Likes:{props.photo.likes}</p>
             </div>
         </>
     )
